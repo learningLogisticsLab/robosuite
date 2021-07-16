073043d966ecb9c6f288cb97b25509e7b7fbc3dc
rojas70@gmail.com <rojas70@gmail.com>
2021-07-16 11:47:49 +0800

    reinserted missing classes from original robosuite

--- robosuite/models/objects/__init__.py
+++ robosuite/models/objects/__init__.py
@@ -2,6 +2,23 @@ from .objects import MujocoObject, MujocoXMLObject, MujocoGeneratedObject
 from .generated_objects import CompositeBodyObject, CompositeObject, PrimitiveObject
 
 from .xml_objects import(
+    BottleObject,
+    CanObject,
+    MasterChefCanObject,
+    LemonObject,
+    MilkObject,
+    BreadObject,
+    CerealObject,
+    SquareNutObject,
+    RoundNutObject,
+    MilkVisualObject,
+    BreadVisualObject,
+    CerealVisualObject,
+    CanVisualObject,
+    MasterChefCanVisualObject,
+    PlateWithHoleObject,
+    DoorObject,
+#--------------------------------        
     o0029,
     o0029v,
     o0001,
