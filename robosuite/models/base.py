2c32b7285cc17570b0f08407170235c0a88fa179
HansenCugito <79178662+HansenCugito@users.noreply.github.com>
2021-08-10 17:13:45 +0800

    Add x_radius, y_radius
    -horizontal_site_pos is defined as 'x_radius y_radius 0'
    -useful to get object orientation

--- robosuite/models/base.py
+++ robosuite/models/base.py
@@ -480,6 +480,23 @@ class MujocoModel(object):
         """
         raise NotImplementedError
 
+    def x_radius(self):
+        """
+        Returns x in (x,y,z) format where x refers to the position in mujoco @ excel database.
+
+        Added to encode object orientation when performing computations. if x > y, then object  is wider.
+        """
+        raise NotImplementedError
+
+    @property
+    def y_radius(self):
+        """
+        Returns y in (x,y,z) format where y refers to the position in mujoco @ excel database.
+
+        Added to encode object orientation when performing computations. if x > y, then object  is wider.
+        """
+        raise NotImplementedError
+
 
 class MujocoXMLModel(MujocoXML, MujocoModel):
     """
@@ -668,4 +685,12 @@ class MujocoXMLModel(MujocoXML, MujocoModel):
     @property
     def vertical_radius(self):
         raise NotImplementedError
+
+    @property
+    def x_radius(self):
+        raise NotImplementedError
+
+    @property
+    def y_radius(self):
+        return NotImplementedError
 
