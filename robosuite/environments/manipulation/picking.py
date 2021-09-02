b31979620390a5bf6fd9eb33edf36d3a2514f861
HansenCugito <79178662+HansenCugito@users.noreply.github.com>
2021-09-02 14:34:04 +0800

    Set collision and vis objs in bin1

--- robosuite/environments/manipulation/picking.py
+++ robosuite/environments/manipulation/picking.py
@@ -734,12 +734,12 @@ class Picking(SingleArmEnv):
                 name                            = "placeObjectSampler",             # name for object sampler for each object
                 mujoco_objects                  = self.visual_objects+self.not_yet_considered_visual_objects,
                 x_range                         = [-bin_x_half, bin_x_half],        # This (+ve,-ve) range goes from center to the walls on each side of the bin
-                y_range                         = [-bin_y_half, -bin_y_half*0.8],
+                y_range                         = [-bin_y_half, bin_y_half],
                 rotation                        = None,                             # Add uniform random rotation
                 rotation_axis                   = 'z',                              # Currently only accepts one axis. TODO: extend to multiple axes.
                 ensure_object_boundary_in_range = True,
                 ensure_valid_placement          = True,
-                reference_pos                   = self.bin2_pos + self.bin_thickness,
+                reference_pos                   = self.bin1_pos + self.bin_thickness,
                 z_offset                        = 0.10,                             # Set a vertical offset of XXcm above the bin
                 z_offset_prob                   = 0.50,                             # probability with which to set the z_offset
             )
