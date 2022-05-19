ee58bc3fc98d018eca861016c8afac452a420068
Hanziscool <hansencugito@ymail.com>
2022-05-19 15:05:02 +0800

    Update lb and ub to allow wrist rotation, currently set low to -pi and high to +pi

--- robosuite/environments/robot_env.py
+++ robosuite/environments/robot_env.py
@@ -269,10 +269,10 @@ class RobotEnv(MujocoEnv):
             if self.top_down_grasp:
                 low[3] = 0
                 low[4] = 0
-                low[5] = 0
+                low[5] = -np.pi
                 high[3] = 0
                 high[4] = 0
-                high[5] = 0
+                high[5] = np.pi
         return low, high
 
     @property
